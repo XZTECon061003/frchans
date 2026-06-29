ff9493f43523821c40abc3b77a260546a786a8f1
XZTECon061003 <2059334648@qq.com>
2026-06-30 01:42:17 +0800

    Updated the website structure.

---
+++ script/menu.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("menuButton");
+const menu = document.getElementById("popupMenu");
+
+console.log(button);
+console.log(menu);
+
+button.addEventListener("click", function (e) {
+
+    e.stopPropagation();
+
+    menu.classList.toggle("show");
+
+});
+
+document.addEventListener("click", function () {
+
+    menu.classList.remove("show");
+
+});
+
+menu.addEventListener("click", function (e) {
+
+    e.stopPropagation();
+
+});
